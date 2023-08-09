e33e2e082cee133b5e84e78a06bc3dd9fa899f5d
me12free <johnewoi72@gmail.com>
2023-08-09 15:19:06 -0700

    12. Fizz Buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,39 +1,12 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-/**
- * insert_node - inserts a number in an ordered linked list
- * @head: double pointer to the linked list
- * @number: number to insert in the new node
- *
- * Return: address of the new node, or NULL
- */
-listint_t *insert_node(listint_t **head, int number)
-{
-	listint_t *new, *tmp, *prev;
-
-	tmp = *head;
-	new = malloc(sizeof(listint_t));
-	if (!new)
-		return (NULL);
-	new->n = number;
-	if (!*head || (*head)->n > number)
-	{
-		new->next = *head;
-		*head = new;
-		return (*head);
-	}
-	while (tmp)
-	{
-		if (tmp->n > number)
-		{
-			break;
-		}
-		prev = tmp;
-		tmp = tmp->next;
-	}
-	new->next = tmp;
-	prev->next = new;
-	return (*head);
-}
+#!/usr/bin/python3
+def fizzbuzz():
+    i = 1
+    while i <= 100:
+        if i % 3 == 0:
+            print('Fizz', end='')
+        if i % 5 == 0:
+            print('Buzz', end='')
+        if i % 3 != 0 and i % 5 != 0:
+            print('{:d}'.format(i), end='')
+        print(" ", end='')
+        i = i + 1
