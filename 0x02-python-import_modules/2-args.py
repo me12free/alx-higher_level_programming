3b701d1e70abaeeb15c0283fea428f16c001161e
me12free <johnewoi72@gmail.com>
2023-08-11 10:39:03 -0700

    checker correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,17 @@
 import sys
 
 if __name__ == "__main__":
+
     count = len(sys.argv) - 1
+
     if count == 0:
         print("0 arguments.")
     elif count == 1:
-        print("1 argument:")
+        print("{} argument:".format(count))
+        for i in range(count):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
     else:
         print("{} arguments:".format(count))
+
         for i in range(count):
             print("{}: {}".format(i + 1, sys.argv[i + 1]))
