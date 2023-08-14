fd127578571423612b851a49ad8357447551a012
me12free <johnewoi72@gmail.com>
2023-08-14 12:31:20 -0700

    3. Print a list of integers... in reverse!

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+#-------------------------------------------------------------------------------#Write a function that replaces an element of a list at a specific position (like in C).
+         #Prototype: def replace_in_list(my_list, idx, element):
+         #If idx is negative, the function should not modify anything, and returns the original list
+         #If idx is out of range (> of number of element in my_list), the function should not modify anything, and returns the
+         #You are not allowed to import any module
+         #You are not allowed to use try/except
+#-------------------------------------------------------------------------------
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+
+    if idx >= len(my_list):
+        return my_list
+
+    my_list[idx] = element
+    return my_list
