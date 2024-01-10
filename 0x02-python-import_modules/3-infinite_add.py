22de1e1741cbef64040696100da202de6b628408
me12ree <johnewoi72@gmail.com>
2024-01-10 06:52:20 -0800

    bug

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,8 @@
 import sys
 
 if __name__ == "__main__":
-
     result = 0
-    for i in range(len(sys.argv) - 1):
-        result += int(sys.argv[i + 1])
-        print("{}".format(result))
+    for arg in sys.argv[1:]:
+        result += int(arg)
+    print(result)
+
