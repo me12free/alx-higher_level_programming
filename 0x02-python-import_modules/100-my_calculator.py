278dcc6e0ec12cfb543b930ecbffe4e778fa6bce
me12ree <johnewoi72@gmail.com>
2024-01-10 07:02:28 -0800

    Validation

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-
     from calculator_1 import add, sub, mul, div
     import sys
 
@@ -11,10 +10,11 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-        if sys.argv[2] not in list(opt.keys()):
-            print("Unknown operator. Available operators: +, -, * and /")
-            sys.exit(1)
+    if sys.argv[2] not in opt.keys():
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-            a = int(sys.argv[1])
-            b = int(sys.argv[3])
-            print("{} {} {} = {}".format(a, sys.argv[2], b, opt[sys.argv[2]](a, b)))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    result = opt[sys.argv[2]](a, b)
+    print("{} {} {} = {}".format(a, sys.argv[2], b, result))
